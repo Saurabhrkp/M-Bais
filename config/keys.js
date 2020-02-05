57fa0b9894efd4ed92002e68580b403c6a6da47a
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-02-06 00:31:09 +0530

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+// dbPassword = 'mongodb+srv://gymdb:S@urabh1997@gymdb-paqxb.mongodb.net/test?retryWrites=true&w=majority';
+dbPassword = 'mongodb://localhost:27017/mbais';
+module.exports = {
+  mongoURI: dbPassword
+};
