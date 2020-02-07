5a296c421112ca32754a06f70510025f369c603d
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-02-07 17:28:46 +0530

    Restart

--- app.js
+++ app.js
@@ -54,6 +54,4 @@ app.use(function(req, res, next) {
 app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+module.exports = app;
