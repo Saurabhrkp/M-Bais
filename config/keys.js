ba9c5d44134c639f158f35eb91ade8e2eda6ac36
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-02-24 00:08:50 +0530

    Made improvement

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 // dburl = 'mongodb+srv://gymdb:S@urabh1997@gymdb-paqxb.mongodb.net/test?retryWrites=true&w=majority';
-dburl = 'mongodb://localhost:27017/mbais';
-// dburl =
-//   'mongodb+srv://gymdb:password21@gymdb-paqxb.mongodb.net/test?retryWrites=true';
+// dburl = 'mongodb://localhost:27017/mbais';
+dburl =
+  'mongodb+srv://gymdb:password21@gymdb-paqxb.mongodb.net/test?retryWrites=true';
 module.exports = {
   mongoURI: dburl
 };
