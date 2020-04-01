4c63da6a1b5ff26997ab2680586e6f7f0139aa2f
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-04-01 23:43:54 +0530

    Updated path and var

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,26 +4,7 @@ const Post = require('../models/Post');
 var async = require('async');
 
 // DB Config
-const connection = require('../database').connection;
-
-// Grid fs
-const Grid = require('gridfs-stream');
-eval(
-  `Grid.prototype.findOne = ${Grid.prototype.findOne
-    .toString()
-    .replace('nextObject', 'next')}`
-);
-
-// Connect GridFS and Mongo
-Grid.mongo = mongoose.mongo;
-
-// Init gfs
-var gfs;
-
-connection.on('open', () => {
-  // Init stream
-  gfs = Grid(connection.db);
-});
+const { bucket } = require('../database');
 
 exports.welcome = function(req, res, next) {
   res.render('welcome', { page: { title: 'Welcome to M-Bias' }, user: false });
@@ -106,7 +87,10 @@ exports.getOne = function(req, res, next) {
     }
 
     // If the file exists then check whether it is an image
-    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+    if (
+      file.metadata.contentType === 'image/jpeg' ||
+      file.metadata.contentType === 'image/png'
+    ) {
       // Read output to browser
       const readstream = gfs.createReadStream(file);
       readstream.pipe(res);
@@ -129,15 +113,15 @@ exports.show = function(req, res, next) {
 
     // If the file exists then check whether it is an image
     if (
-      file.contentType === 'image/jpeg' ||
-      file.contentType === 'image/png' ||
-      file.contentType === 'video/mp4'
+      file.metadata.contentType === 'image/jpeg' ||
+      file.metadata.contentType === 'image/png' ||
+      file.metadata.contentType === 'video/mp4'
     ) {
       // Read output to browser
       const readstream = gfs.createReadStream(file);
       readstream.pipe(res);
       res.render('video', {
-        page: { title: file.metadata[0] },
+        page: { title: file.metadata.metadata.subject },
         files: file,
         user: req.user
       });
@@ -160,9 +144,9 @@ exports.play = function(req, res, next) {
 
     // If the file exists then check whether it is an image
     if (
-      file.contentType === 'image/jpeg' ||
-      file.contentType === 'image/png' ||
-      file.contentType === 'video/mp4'
+      file.metadata.contentType === 'image/jpeg' ||
+      file.metadata.contentType === 'image/png' ||
+      file.metadata.contentType === 'video/mp4'
     ) {
       // Read output to browser
       StreamGridFile(req, res, file);
@@ -301,62 +285,62 @@ exports.post_post = function(req, res) {
   }
 };
 
-function StreamGridFile(req, res, files) {
-  var file = files.filename;
-  gfs.findOne(
-    {
-      filename: file
-    },
-    function(err, file) {
-      if (err) {
-        return res.status(400).send({
-          err: errorHandler.getErrorMessage(err)
-        });
-      }
-      if (!file) {
-        return res.status(404).send({
-          err: 'No se encontró el registro especificado.'
-        });
-      }
+// function StreamGridFile(req, res, files) {
+//   var file = files.filename;
+//   gfs.findOne(
+//     {
+//       filename: file
+//     },
+//     function(err, file) {
+//       if (err) {
+//         return res.status(400).send({
+//           err: errorHandler.getErrorMessage(err)
+//         });
+//       }
+//       if (!file) {
+//         return res.status(404).send({
+//           err: 'No se encontró el registro especificado.'
+//         });
+//       }
 
-      if (req.headers['range']) {
-        var parts = req.headers['range'].replace(/bytes=/, '').split('-');
-        var partialstart = parts[0];
-        var partialend = parts[1];
+//       if (req.headers['range']) {
+//         var parts = req.headers['range'].replace(/bytes=/, '').split('-');
+//         var partialstart = parts[0];
+//         var partialend = parts[1];
 
-        var start = parseInt(partialstart, 10);
-        var end = partialend ? parseInt(partialend, 10) : file.length - 1;
-        var chunksize = end - start + 1;
+//         var start = parseInt(partialstart, 10);
+//         var end = partialend ? parseInt(partialend, 10) : file.length - 1;
+//         var chunksize = end - start + 1;
 
-        res.writeHead(206, {
-          'Accept-Ranges': 'bytes',
-          'Content-Length': chunksize,
-          'Content-Range': 'bytes ' + start + '-' + end + '/' + file.length,
-          'Content-Type': file.contentType
-        });
+//         res.writeHead(206, {
+//           'Accept-Ranges': 'bytes',
+//           'Content-Length': chunksize,
+//           'Content-Range': 'bytes ' + start + '-' + end + '/' + file.length,
+//           'Content-Type': file.contentType
+//         });
 
-        gfs
-          .createReadStream({
-            _id: file._id,
-            range: {
-              startPos: start,
-              endPos: end
-            }
-          })
-          .pipe(res);
-      } else {
-        res.header('Content-Length', file.length);
-        res.header('Content-Type', file.contentType);
+//         gfs
+//           .createReadStream({
+//             _id: file._id,
+//             range: {
+//               startPos: start,
+//               endPos: end
+//             }
+//           })
+//           .pipe(res);
+//       } else {
+//         res.header('Content-Length', file.length);
+//         res.header('Content-Type', file.metadata.contentType);
 
-        gfs
-          .createReadStream({
-            _id: file._id
-          })
-          .pipe(res);
-      }
-    }
-  );
-}
+//         gfs
+//           .createReadStream({
+//             _id: file._id
+//           })
+//           .pipe(res);
+//       }
+//     }
+//   );
+// }
 
 function escapeRegex(text) {
   return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
