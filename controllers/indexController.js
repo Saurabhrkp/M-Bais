f03e8630f26f9d14a414456659365ac86829fc3d
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-04-02 16:28:37 +0530

    Converted to use arrow functions and new schema

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,59 +1,67 @@
-const mongoose = require('mongoose');
+// Load User model
 const User = require('../models/User');
+// Load Video model
+const Video = require('../models/Video');
+// Load Post model
 const Post = require('../models/Post');
 var async = require('async');
+const { StreamCloudFile, escapeRegex } = require('./gcsHelper');
 
 // DB Config
 const { bucket } = require('../database');
 
-exports.welcome = function(req, res, next) {
+exports.welcome = (req, res, next) => {
   res.render('welcome', { page: { title: 'Welcome to M-Bias' }, user: false });
 };
 
-exports.dashboard = function(req, res, next) {
+exports.dashboard = (req, res, next) => {
   res.render('dashboard', {
     page: { title: 'Your Dashboard || M-Bias' },
     user: req.user
   });
 };
 
-exports.contact = function(req, res, next) {
+exports.contact = (req, res, next) => {
   res.render('contact', { page: { title: 'Contact us' }, user: false });
 };
 
-exports.search = function(req, res, next) {
+exports.search = (req, res, next) => {
   const regex = new RegExp(escapeRegex(req.query.search), 'gi');
-  gfs.files.findOne({ aliases: regex }, (err, file) => {
-    // Check if the input is a valid image or not
-    if (!file || file.length === 0) {
-      return res.render('result', {
-        page: {
-          title: 'Search results not found|| M-Bias',
-          search: req.query.search
-        },
-        files: false,
+  Video.findOne({ aliases: regex }).then(video => {
+    const files = bucket.file(video.filename);
+    files.get().then(data => {
+      const file = data[0];
+      // Check if the input is a valid image or not
+      if (!file || file.metadata.size === 0) {
+        return res.render('result', {
+          page: {
+            title: 'Search results not found|| M-Bias',
+            search: req.query.search
+          },
+          files: false,
+          user: req.user
+        });
+      }
+      res.render('result', {
+        page: { title: 'Search results || M-Bias', search: req.query.search },
+        files: file,
         user: req.user
       });
-    }
-    res.render('result', {
-      page: { title: 'Search results || M-Bias', search: req.query.search },
-      files: file,
-      user: req.user
     });
   });
 };
 
-exports.searchPost = function(req, res, next) {
+exports.searchPost = (req, res, next) => {
   const regex = new RegExp(escapeRegex(req.query.search), 'gi');
   async.parallel(
     {
-      post: function(callback) {
+      post: callback => {
         Post.find({ aliases: regex })
           .populate('_user')
           .exec(callback);
       }
     },
-    function(err, results) {
+    (err, results) => {
       if (err) {
         return next(err);
       }
@@ -77,35 +85,76 @@ exports.searchPost = function(req, res, next) {
   );
 };
 
-exports.getOne = function(req, res, next) {
-  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-    // Check if the input is a valid image or not
-    if (!file || file.length === 0) {
-      return res.status(404).json({
-        err: 'No file exists'
-      });
-    }
+exports.getOne = (req, res, next) => {
+  Video.findOne({ filename: req.params.filename }).then(video => {
+    const files = bucket.file(video.filename);
+    files.get().then(data => {
+      const file = data[0];
+      // Check if the input is a valid image or not
+      if (!file || file.metadata.size === 0) {
+        return res.status(404).json({
+          err: 'No file exists'
+        });
+      }
 
-    // If the file exists then check whether it is an image
-    if (
-      file.metadata.contentType === 'image/jpeg' ||
-      file.metadata.contentType === 'image/png'
-    ) {
-      // Read output to browser
-      const readstream = gfs.createReadStream(file);
-      readstream.pipe(res);
-    } else {
-      res.status(404).json({
-        err: 'Not an image'
-      });
-    }
+      // If the file exists then check whether it is an image
+      if (
+        file.metadata.contentType === 'image/jpeg' ||
+        file.metadata.contentType === 'image/png'
+      ) {
+        // Read output to browser
+        const readstream = file.createReadStream();
+        readstream.pipe(res);
+      } else {
+        res.status(404).json({
+          err: 'Not an image'
+        });
+      }
+    });
   });
 };
 
-exports.show = function(req, res, next) {
-  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+exports.show = (req, res, next) => {
+  Video.findOne({ filename: req.params.filename }).then(video => {
+    const files = bucket.file(video.filename);
+    files.get().then(data => {
+      const file = data[0];
+      // Check if the input is a valid image or not
+      if (!file || file.metadata.size === 0) {
+        return res.status(404).json({
+          err: 'No file exists'
+        });
+      }
+
+      // If the file exists then check whether it is an image
+      if (
+        file.metadata.contentType === 'image/jpeg' ||
+        file.metadata.contentType === 'image/png' ||
+        file.metadata.contentType === 'video/mp4'
+      ) {
+        // Read output to browser
+        const readstream = file.createReadStream();
+        readstream.pipe(res);
+        res.render('video', {
+          page: { title: file.metadata.metadata.subject },
+          files: file,
+          user: req.user
+        });
+      } else {
+        res.status(404).json({
+          err: 'Not available'
+        });
+      }
+    });
+  });
+};
+
+exports.play = (req, res, next) => {
+  const files = bucket.file(req.params.filename);
+  files.get().then(data => {
+    const file = data[0];
     // Check if the input is a valid image or not
-    if (!file || file.length === 0) {
+    if (!file || file.metadata.size === 0) {
       return res.status(404).json({
         err: 'No file exists'
       });
@@ -118,38 +167,7 @@ exports.show = function(req, res, next) {
       file.metadata.contentType === 'video/mp4'
     ) {
       // Read output to browser
-      const readstream = gfs.createReadStream(file);
-      readstream.pipe(res);
-      res.render('video', {
-        page: { title: file.metadata.metadata.subject },
-        files: file,
-        user: req.user
-      });
-    } else {
-      res.status(404).json({
-        err: 'Not available'
-      });
-    }
-  });
-};
-
-exports.play = function(req, res, next) {
-  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-    // Check if the input is a valid image or not
-    if (!file || file.length === 0) {
-      return res.status(404).json({
-        err: 'No file exists'
-      });
-    }
-
-    // If the file exists then check whether it is an image
-    if (
-      file.metadata.contentType === 'image/jpeg' ||
-      file.metadata.contentType === 'image/png' ||
-      file.metadata.contentType === 'video/mp4'
-    ) {
-      // Read output to browser
-      StreamGridFile(req, res, file);
+      StreamCloudFile(req, res, file);
     } else {
       res.status(404).json({
         err: 'Not available'
@@ -159,16 +177,16 @@ exports.play = function(req, res, next) {
 };
 
 // Display detail page for a specific Pdf.
-exports.onepost = function(req, res, next) {
+exports.onepost = (req, res, next) => {
   async.parallel(
     {
-      post: function(callback) {
+      post: callback => {
         Post.findById(req.params.id)
           .populate('_user')
           .exec(callback);
       }
     },
-    function(err, results) {
+    (err, results) => {
       if (err) {
         return next(err);
       }
@@ -190,43 +208,43 @@ exports.onepost = function(req, res, next) {
   );
 };
 
-exports.delete = function(req, res, next) {
+exports.delete = (req, res, next) => {
   const ID = req.params.id;
   const UserID = req.user.id;
-  async.parallel({}, function(err, results) {
+  async.parallel({}, (err, results) => {
     if (err) {
       return next(err);
     } else {
-      User.findOneAndUpdate(UserID, { $pull: { posts: ID } }, function(err) {
+      User.findOneAndUpdate(UserID, { $pull: { posts: ID } }, err => {
         if (err) {
           return res.status(500).json({ error: 'error in deleting address' });
         }
       });
       Post.findOneAndDelete(
         req.params.id,
-        function deletephoto(err) {
+        (deletephoto = err => {
           if (err) {
             return next(err);
           }
           req.flash('success_msg', 'Succesfully deleted');
           res.redirect('/dashboard');
-        },
+        }),
         console.log(`Deleted from photos`)
       );
     }
   });
 };
 
-exports.allpost = function(req, res) {
+exports.allpost = (req, res) => {
   async.parallel(
     {
-      posts: function(callback) {
+      posts: callback => {
         Post.find()
           .populate('_user')
           .exec(callback);
       }
     },
-    function(err, results) {
+    (err, results) => {
       if (err) {
         return next(err);
       }
@@ -244,14 +262,14 @@ exports.allpost = function(req, res) {
   );
 };
 
-exports.post_get = function(req, res) {
+exports.post_get = (req, res) => {
   res.render('makepost', {
     page: { title: 'Make a new Post' },
     user: req.user
   });
 };
 
-exports.post_post = function(req, res) {
+exports.post_post = (req, res) => {
   const { subject, message, aliases } = req.body;
   const errors = [];
   if (!subject || !message) {
@@ -272,9 +290,9 @@ exports.post_post = function(req, res) {
       aliases,
       _user: req.user._id
     });
-    post.save(function(err, post) {
+    post.save((err, post) => {
       if (err) return res.send(err);
-      User.findById(req.user._id, function(err, user) {
+      User.findById(req.user._id, (err, user) => {
         if (err) return res.send(err);
         user.posts.push(post._id);
         user.save();
@@ -284,64 +302,3 @@ exports.post_post = function(req, res) {
     });
   }
 };
-
-// function StreamGridFile(req, res, files) {
-//   var file = files.filename;
-//   gfs.findOne(
-//     {
-//       filename: file
-//     },
-//     function(err, file) {
-//       if (err) {
-//         return res.status(400).send({
-//           err: errorHandler.getErrorMessage(err)
-//         });
-//       }
-//       if (!file) {
-//         return res.status(404).send({
-//           err: 'No se encontró el registro especificado.'
-//         });
-//       }
-
-//       if (req.headers['range']) {
-//         var parts = req.headers['range'].replace(/bytes=/, '').split('-');
-//         var partialstart = parts[0];
-//         var partialend = parts[1];
-
-//         var start = parseInt(partialstart, 10);
-//         var end = partialend ? parseInt(partialend, 10) : file.length - 1;
-//         var chunksize = end - start + 1;
-
-//         res.writeHead(206, {
-//           'Accept-Ranges': 'bytes',
-//           'Content-Length': chunksize,
-//           'Content-Range': 'bytes ' + start + '-' + end + '/' + file.length,
-//           'Content-Type': file.contentType
-//         });
-
-//         gfs
-//           .createReadStream({
-//             _id: file._id,
-//             range: {
-//               startPos: start,
-//               endPos: end
-//             }
-//           })
-//           .pipe(res);
-//       } else {
-//         res.header('Content-Length', file.length);
-//         res.header('Content-Type', file.metadata.contentType);
-
-//         gfs
-//           .createReadStream({
-//             _id: file._id
-//           })
-//           .pipe(res);
-//       }
-//     }
-//   );
-// }
-
-function escapeRegex(text) {
-  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
-}
