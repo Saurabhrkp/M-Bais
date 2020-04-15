4b7ef186e2212b653479ca7549b3c229f3f3da11
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-04-16 00:26:34 +0530

    Changes Folder Structure

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-dburl = 'mongodb://localhost:27017/mbais';
-// dburl =
-//   'mongodb+srv://gymdb:password21@gymdb-paqxb.mongodb.net/dharvesh?retryWrites=true&w=majority';
-
-bucketUrl = 'mech-bais.appspot.com';
-
-export const mongoURI = dburl;
-export const bucketURI = bucketUrl;
