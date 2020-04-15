fef121744a9d6dfef4887b1bb12b80bdf8c3a909
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-04-15 19:57:06 +0530

    Removed Auth

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,8 @@
-// dburl = 'mongodb+srv://gymdb:S@urabh1997@gymdb-paqxb.mongodb.net/test?retryWrites=true&w=majority';
 dburl = 'mongodb://localhost:27017/mbais';
 // dburl =
 //   'mongodb+srv://gymdb:password21@gymdb-paqxb.mongodb.net/dharvesh?retryWrites=true&w=majority';
 
 bucketUrl = 'mech-bais.appspot.com';
 
-module.exports = {
-  mongoURI: dburl,
-  bucketURI: bucketUrl,
-};
+export const mongoURI = dburl;
+export const bucketURI = bucketUrl;
