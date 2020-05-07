f56cc6133ddab49ca8d47fd416921c00909852bf
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-05-08 00:50:46 +0530

    Updated Database file

--- models/database.js
+++ models/database.js
@@ -1,54 +1,25 @@
 const mongoose = require('mongoose');
-var S3 = require('aws-sdk/clients/s3');
-const Multer = require('multer');
+const S3 = require('aws-sdk/clients/s3');
 
 // DB Config
 const { mongoURI, config } = require('../bin/keys');
 
 // Connect to MongoDB
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log(`MongoDB is Connected on ${mongoURI}`))
+  .catch((err) => console.error(`Unable to MongoDB due to ${err.message}`));
 
 // Promise of mongoose
 mongoose.Promise = global.Promise;
-
-// MongoDB Connection
-var connectionURI = mongoose.connection;
-
-// Console logs for connection and error
-connectionURI.on('connected', () =>
-  console.log(`MongoDB is Connected on ${mongoURI}`)
-);
-connectionURI.on(
-  'error',
-  console.error.bind(console, 'MongoDB connection error:')
-);
 
 // Instantiate a storage client
 const S3Client = new S3(config);
 /*  Notice that if you don't provide a dirName, the file will be automatically uploaded to the root of your bucket */
 
-// Multer is required to process file uploads and make them available via
-// req.files.
-const upload = Multer({
-  storage: Multer.memoryStorage(),
-  limits: {
-    fileSize: 50 * 1024 * 1024, // no larger than 20mb, you can change as needed.
-  },
-  fileFilter: (req, file, next) => {
-    if (
-      file.mimetype.startsWith('image/') ||
-      file.mimetype.startsWith('video/')
-    ) {
-      next(null, true);
-    } else {
-      next(null, false);
-    }
-  },
-});
-
-module.exports = { bucket: S3Client, uploadFile: upload };
+module.exports = { bucket: S3Client };
