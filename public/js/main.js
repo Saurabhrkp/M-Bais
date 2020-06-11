dbee3235a2e5277360b532800442caa896c278e1
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-06-12 00:51:28 +0530

    Make profile page for editing profile,

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,22 @@ $(document).on('click', '[data-toggle="lightbox"]', function (event) {
   event.preventDefault();
   $(this).ekkoLightbox();
 });
+
+$(document).ready(function () {
+  $('#profileForm :input').prop('disabled', true);
+  $('#editProfile').prop('disabled', false);
+  $('#submitProfile').hide();
+  $('#editProfile').click(function () {
+    if (this.value == 'Edit Profile') {
+      this.value = 'Cancel';
+      $('#profileForm :input').prop('disabled', false);
+      $('#editProfile').prop('disabled', false);
+      $('#submitProfile').show();
+    } else {
+      this.value = 'Edit Profile';
+      $('#profileForm :input').prop('disabled', true);
+      $('#editProfile').prop('disabled', false);
+      $('#submitProfile').hide();
+    }
+  });
+});
