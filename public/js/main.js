50907d4c52d81533c7335e4c0d13ff8066c728a1
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-06-12 19:39:05 +0530

    Updating avatar

--- public/js/main.js
+++ public/js/main.js
@@ -7,17 +7,20 @@ $(document).ready(function () {
   $('#profileForm :input').prop('disabled', true);
   $('#editProfile').prop('disabled', false);
   $('#submitProfile').hide();
+  $('#avatar').hide();
   $('#editProfile').click(function () {
     if (this.value == 'Edit Profile') {
       this.value = 'Cancel';
       $('#profileForm :input').prop('disabled', false);
       $('#editProfile').prop('disabled', false);
       $('#submitProfile').show();
+      $('#avatar').show();
     } else {
       this.value = 'Edit Profile';
       $('#profileForm :input').prop('disabled', true);
       $('#editProfile').prop('disabled', false);
       $('#submitProfile').hide();
+      $('#avatar').hide();
     }
   });
 });
