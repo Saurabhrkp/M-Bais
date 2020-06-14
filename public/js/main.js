8490d5b998ab53ad8aefa75c2d5077afe6588e4e
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-06-14 23:20:44 +0530

    Added Form to post

--- public/js/main.js
+++ public/js/main.js
@@ -27,3 +27,39 @@ $(document).ready(function () {
     }
   });
 });
+
+$('#video').change(function (e) {
+  if (e.target.files.length) {
+    $(this).next('.custom-file-label').html(e.target.files[0].name);
+  } else {
+    $(this).next('.custom-file-label').html('Choose Video');
+  }
+});
+
+$('#thumbnail').change(function (e) {
+  if (e.target.files.length) {
+    $(this).next('.custom-file-label').html(e.target.files[0].name);
+  } else {
+    $(this).next('.custom-file-label').html('Choose Thumbnail');
+  }
+});
+
+$('#photos').change(function (e) {
+  if (e.target.files.length > 6) {
+    alert('You can select only 6 Photos');
+    $(this).next('.custom-file-label').html('Choose Photos');
+    $(this).val('');
+  } else if (e.target.files.length > 0 && e.target.files.length < 6) {
+    let file_name = [];
+    for (const key in e.target.files) {
+      if (e.target.files.hasOwnProperty(key)) {
+        const element = e.target.files[key];
+        file_name.push(element.name);
+      }
+    }
+    const files = file_name.join(', ');
+    $(this).next('.custom-file-label').html(files);
+  } else {
+    $(this).next('.custom-file-label').html('Choose Photos');
+  }
+});
